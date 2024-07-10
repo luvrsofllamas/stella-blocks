fb2737474e4578bd15c48ba3065b5d0ba2d8c150
Keith Kurak <keith.kurak@gmail.com>
2024-07-09 21:47:42 -0400

    initial commit

---
+++ app/index.tsx
@@ -0,0 +1,83 @@
+import { Canvas, Rect } from "@shopify/react-native-skia";
+import { useState } from "react";
+import { View, useWindowDimensions, Text } from "react-native";
+
+function constrainValue(value) {
+  return Math.max(Math.min(value, 9), 0);
+}
+
+const Button = ({ onPress, title, orientation }) => (
+  <View
+    style={{
+      width: orientation === "vertical" ? 75 : 100,
+      height: orientation === "vertical" ? 100 : 75,
+      backgroundColor: "blue",
+      justifyContent: "center",
+      alignItems: "center",
+    }}
+    onTouchEnd={onPress}
+  >
+    <Text style={{ color: "white" }}>{title}</Text>
+  </View>
+);
+
+export default function Index() {
+  const windowDimensions = useWindowDimensions();
+
+  const gameField = {
+    width: windowDimensions.width - 20,
+    height: windowDimensions.width - 20,
+    blockSize: (windowDimensions.width - 20) / 10,
+  };
+
+  const [player, setPlayer] = useState({
+    x: 0,
+    y: 0,
+    size: gameField.blockSize,
+  });
+
+  const buttons = {
+    up: () => setPlayer({ ...player, y: constrainValue(player.y - 1) }),
+    down: () => setPlayer({ ...player, y: constrainValue(player.y + 1) }),
+    left: () => setPlayer({ ...player, x: constrainValue(player.x - 1) }),
+    right: () => setPlayer({ ...player, x: constrainValue(player.x + 1) }),
+  };
+
+  return (
+    <View style={{ flex: 1, backgroundColor: "gray", paddingTop: 80 }}>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: 'center' }}>
+        <Canvas style={{ backgroundColor: 'white', height: gameField.height, width: gameField.width }}>
+          <Rect
+            height={player.size}
+            width={player.size}
+            x={player.x * player.size}
+            y={player.y * player.size}
+            color="red"
+          />
+        </Canvas>
+      </View>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Button onPress={buttons.up} title="Up" orientation="vertical" />
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
+          <Button
+            onPress={buttons.left}
+            title="Left"
+            orientation="horizontal"
+          />
+          <View style={{ width: 75 }}></View>
+          <Button
+            onPress={buttons.right}
+            title="Right"
+            orientation="horizontal"
+          />
+        </View>
+        <Button onPress={buttons.down} title="Down" orientation="vertical" />
+      </View>
+    </View>
+  );
+}
